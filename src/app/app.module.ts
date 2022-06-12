7d849bc60a1ddf3605b54ecbe706c6e8e40e24c8
Jakob Pachtrog <jakob.pachtrog@gmail.com>
2022-06-12 18:46:35 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
